e195d3318700714fd838bf5e3eff58255b08e752
Sirojbek Azimjonov <joris5969@gmail.com>
2024-03-24 01:46:11 +0500

    add auto versioning

--- TaskCopeCommon/TaskCopeCommon/Models/AuthenticateResponse.cs
+++ TaskCopeCommon/TaskCopeCommon/Models/AuthenticateResponse.cs
@@ -5,7 +5,7 @@ namespace TaskCopeCommon.Models
     public class AuthenticateResponse
     {
         public int Id { get; set; }
-        public string Fullname { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
         public string Token { get; set; }
@@ -13,7 +13,7 @@ namespace TaskCopeCommon.Models
         public AuthenticateResponse(User user, string token)
         {
             Id = user.Id;
-            Fullname = user.FullName;
+            FullName = user.FullName;
             Email = user.Email;
             Role = user.Role.Name;
             Token = token;
